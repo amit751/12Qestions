667bd2250999ddc0900c8755cd18ac23d850fb17
amit <amitoosh.mor@gmail.com>
2021-01-18 16:39:16 +0200

    q4

--- q4.js
+++ q4.js
@@ -1,20 +1,16 @@
+const store =[""];
+store[0] = Number(prompt("hi plese type 5 numbers,the first:"));
+store[1] = Number(prompt("the second:"));
+store[2] = Number(prompt("the third:"));
+store[3] = Number(prompt("the 4th:"));
+store[4] = Number(prompt("the 5th:"));
+let max = store[0];
 
-
-let store =[""];
- store[0] = prompt("hi plese type 5 numbers,the first:");
- store[1] = prompt("the second:");
- store[2] = prompt("the third:");
- store[3] = prompt("the 4th:");
- store[4] = prompt("the 5th:");
-
- let max = store[0];
-
- for( let i=1 ; i<5 ; i++){
-     if( store[i]>max){
-         max= store[i];
+for( let i=0 ; i<5 ; i++ ){
+    if( store[i] > max ){
+        max= store[i];
      }
-    
- }
-alert(max);
+}
 
+alert(max);
 
