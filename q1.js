02a07ca777f6da06a035adf4a60be5a4c26d75d1
amit <amitoosh.mor@gmail.com>
2021-01-18 13:09:22 +0200

    q1

---
+++ q1.js
@@ -0,0 +1,12 @@
+
+
+let number1 = prompt("hi plese type 2 numbers,the first:");
+let number2 =prompt("the second:");
+
+if(Number.number1>Number.number2){
+    alert(number1);
+
+}
+else{
+    alert(number2);
+}
