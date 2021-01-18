211ec228d3ae5ca373974d75446c201b13903f2e
amit <amitoosh.mor@gmail.com>
2021-01-18 13:30:56 +0200

    q2

---
+++ q2.js
@@ -0,0 +1,16 @@
+let number1 = prompt("hi plese type 3 numbers,the first:");
+let number2 =prompt("the second:");
+let number3 =prompt("the third:");
+
+Number.number1;
+Number.number2;
+Number.number3;
+let sum = number1+number2+number3;
+
+if(sum>0){
+    alert("the sign is +");
+
+}
+else{
+    alert("the sign is -");
+}
